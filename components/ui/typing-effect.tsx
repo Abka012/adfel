2bdf33d9e2b3509469d0dc9857e9ec64ef043ba8
abka <abka@localhost.localdomain>
2025-03-22 20:52:41 -0400

    new packages install

--- components/ui/typing-effect.tsx
+++ components/ui/typing-effect.tsx
@@ -6,7 +6,7 @@ const CustomTypingEffect: React.FC = () => {
   const [loopNum, setLoopNum] = useState<number>(0);
   const [typingSpeed, setTypingSpeed] = useState<number>(150);
 
-  const words: string[] = ['This is Advent Fellowship', 'Know as', 'ADFEL!!!'];
+  const words: string[] = ['Welcome to Advent Fellowship', 'Your Home Away From Home'];
   const currentWord: string = words[loopNum % words.length];
 
   useEffect(() => {
